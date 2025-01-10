16df45dd1cd41c95bc3dbb87a42c1d836d7b572b
powerSeries <joseesotolongo@gmail.com>
2025-01-10 01:08:23 -0500

    producer setup

---
+++ KafkaDotNet.ProducerApi/ProducerService.cs
@@ -0,0 +1,43 @@
+﻿using Confluent.Kafka;
+
+namespace KafkaDotNet.ProducerApi
+{
+    public class ProducerService
+    {
+        private readonly ILogger<ProducerService> _logger;
+        public ProducerService(ILogger<ProducerService> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task ProduceAsync(CancellationToken token)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "localhost:9092",
+                AllowAutoCreateTopics = true,
+                Acks = Acks.All
+            };
+
+            using var producer = new ProducerBuilder<Null, string>(config).Build();
+
+            try
+            {
+                var result = await producer.ProduceAsync(topic: "test-topic", 
+                    new Message<Null, string> 
+                    { 
+                        Value = $"{DateTime.UtcNow} - Hello World!" 
+                    }, 
+                    token);
+                _logger.LogInformation($"Delivered message!\nValue: {result.Value}\nOffset: {result.TopicPartitionOffset}");
+            }
+            catch (ProduceException<Null, string> e)
+            {
+                _logger.LogError($"Delivery failed: {e.Error.Reason}");
+            }
+
+            producer.Flush(token);
+            producer.Dispose();
+        }
+    }
+}
